134ca3cf878d4090a9129a5493f4a56dbb40821a
Matthew Spriggs <matspriggs@gmail.com>
2026-01-15 09:35:14 +0000

    added pract 5 falcon sub

---
+++ gamod/practical5/falcon/salmonella/2025_01_14_falcon_qsub_v1_0_0.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Example of a script for Crescent2 batch job submission 
+# Matthew Spriggs, 14Jan2026
+
+# Crescent2 script
+# Note: this script should be run on a compute node
+# qsub script.sh
+
+# PBS directives
+#---------------
+
+#PBS -N 2025_01_14_falcon_qsub_v1_0_0
+#PBS -l nodes=1:ncpus=4
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M Matthew.Spriggs.452@cranfield.ac.uk
+
+#===============
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps/modules/all
+## =============
+
+# --- Your code starts here --- #
+
+# Stop at runtime errors
+set -e
+
+# Start message (this is an example, change it!)
+echo "Started script"
+date
+echo ""
+
+# Load required modules (this is an example, change it if needed!)
+module load Singularity/3.11.0-1-system
+singularity --version
+
+# Working folder (this is an example, change it if needed!)
+working_folder="/mnt/beegfs/home/s430452/advanced_sequencing_informatics_and_genome_assembly/gamod/"
+cd "${working_folder}"
+
+# File names (this is an example, change it if needed!)
+
+input_file="${working_folder}/data/salmonella/SRR8066536_15perc.fastq.gz"
+falcon_dir="${working_folder}/practical5/falcon/salmonella/"
+fasta_name="SRR8066536_15perc.fasta"
+    
+# FALCON manipulation
+zcat ${input_file} | \
+    awk '{if(NR %4 <3){if(NR % 4 == 1){split($3,a,"length="); 
+    print ">"substr($1,2,length($1))"/"NR"/0_"a[2]}
+    else {if(NR % 4 == 2){print}}}}' >\
+   ${falcon_dir}/${fasta_name}
+
+ls ${falcon_dir}/${fasta_name} > inputFof
+
+# Run job in singularity container (this is an example, change it if needed!)
+singularity exec ${working_folder}/gamod.simg \
+    bash -c "source /Falcon-integrate/fc_env/bin/activate && fc_run ${falcon_dir}/falcon_config"
+
+
+# Completion message
+echo "Done"
+date
+
+# Do not forget to update the header and PBS directives:
+# e.g. the job name, e-mail address,
+# num of cores, required time and cluster queue, 
+# Also, do not to forget to load and check the required modules.
+
+## Tidy up
+## =========================
+rm $PBS_O_WORKDIR/$PBS_JOBID
+
