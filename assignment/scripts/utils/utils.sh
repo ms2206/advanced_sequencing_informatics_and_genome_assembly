1c414fad5732d354880bdddee619869eb8c97b5b
Matthew Spriggs <matspriggs@gmail.com>
2026-01-21 07:48:08 +0000

    add: include functions for counting reads and bases in unzipped fastq files

--- assignment/scripts/utils/utils.sh
+++ assignment/scripts/utils/utils.sh
@@ -15,4 +15,23 @@ function calculate_read_length {
     fastq_file=$1
     read_len=$(zcat $fastq_file | awk 'NR % 4 == 2' | head -n1 | awk '{print length}')
     echo $read_len
-}   
+}
+
+# count the number of reads in the fastq file not zipped
+function unziped_count_fastq_reads {
+    fastq_file=$1
+    zcat $fastq_file | grep "^@" | wc -l
+} 
+
+# count the number of bases in the fastq file not zipped
+function unziped_count_fastq_bases {
+    fastq_file=$1
+    zcat $fastq_file | grep -v "^@\|+" | awk '{if(NR%2==1) {print}}' | awk '{total += length($1)} END {print "Sum: "total}'
+}
+ 
+# calculate read length from fastq file not zipped
+function unziped_calculate_read_length {
+    fastq_file=$1
+    read_len=$(zcat $fastq_file | awk 'NR % 4 == 2' | head -n1 | awk '{print length}')
+    echo $read_len
+}
