82072cfe392cbc2c971ac9b4e68d505d9e9c7e84
Matthew Spriggs <matspriggs@gmail.com>
2026-01-22 09:03:50 +0000

    fix: update count_reads_above_length function to use strict length comparison

--- assignment/scripts/utils/utils.sh
+++ assignment/scripts/utils/utils.sh
@@ -40,6 +40,6 @@ function unziped_calculate_read_length {
 function count_reads_above_length {
     fastq_file=$1
     length_threshold=$2
-    count=$(zcat $fastq_file | awk 'NR % 4 == 2' | awk '{print length($0)}' | awk -v threshold="$length_threshold" '$1 >= threshold' | wc -l)
-    echo "File: $fastq_file - Number of reads with length >= $length_threshold: $count"
+    count=$(zcat $fastq_file | awk 'NR % 4 == 2' | awk '{print length($0)}' | awk -v threshold="$length_threshold" '$1 > threshold' | wc -l)
+    echo "File: $fastq_file - Number of reads with length > $length_threshold: $count"
 }
