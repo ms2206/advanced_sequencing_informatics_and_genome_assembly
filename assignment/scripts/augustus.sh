286689bb465827a2e4b8e45d4f52322a1cb53d35
Matthew Spriggs <matspriggs@gmail.com>
2026-01-25 15:42:54 +0000

    update kmer size retrieval in dbg2olc script; add new augustus script

--- assignment/scripts/augustus.sh
+++ assignment/scripts/augustus.sh
@@ -11,7 +11,7 @@
 # PBS directives
 #---------------
 
-#PBS -N dbg2olc
+#PBS -N augustus
 #PBS -l nodes=1:ncpus=4
 #PBS -l walltime=00:30:00
 #PBS -q half_hour
@@ -50,6 +50,7 @@ echo ""
 
 # Load required modules (this is an example, change it if needed!)
 module load Singularity/3.11.0-1-system
+module load AUGUSTUS/3.5.0-foss-2023a
 singularity --version
 
 # Working folder (this is an example, change it if needed!)
@@ -60,75 +61,20 @@ cd "${WORKING_FOLDER}"
 source "${WORKING_FOLDER}/scripts/filepaths.txt"
 
 # Any additional variables or paths can be added here
-# get sample name
-sample=$(basename "${COR_ILLUMINA_SR_READ_1}" .pair_1.fq.gz)
-
-# dynamically get kmer size from SOAP assembly path
-kmer=${input_kmer_size}
-
-# Create output directory and change to it
-output_dir="${DBG2OLC_DIR}/${sample}_k${kmer}"
-mkdir -p "${output_dir}"
-cd "${output_dir}"
-
-# Sanity checks
-if [[ ! -s "${SOAP_ASSEMBELLY}" ]]; then
-    echo "ERROR: Contigs file not found or empty: ${SOAP_ASSEMBELLY}" 
-    exit 1
-fi
-
-if [[ ! -s "${PACBIO_READS}" ]]; then
-    echo "ERROR: PacBio reads file not found or empty: ${PACBIO_READS}" 
-    exit 1
-fi
-
+#<-- your variables ...>
+    
 # Main code 
 # ========================
-echo "Running DBG2OLC for sample ${sample} with kmer size ${kmer}"
-echo "Results will be saved to: ${output_dir}"
+# <-- your code ...>
+augustus \
+    --species=human \
+    --gff3=on \
+    genome.scf.fasta > masurca_genes.gff3
 
-# Unzip reads
-zcat "${PACBIO_READS}" > "HS7_pacbioData.fasta"
-
-
-# Run DBG2OLC using my soapdenovo2 assembly
-singularity exec ${SINGULARITY} \
-    "${DBG2OLC}/DBG2OLC" \
-    -k ${kmer} \
-    AdaptiveTh 0.0001 \
-    KmerCovTh 2 \
-    MinOverlap 20 \
-    RemoveChimera 1 \
-    Contigs "${SOAP_ASSEMBELLY}" \
-    f "HS7_pacbioData.fasta"
-
-# remove intermediate file
+# Count genes
+grep -c $'\tgene\t' masurca_genes.gff3
 
 
-echo "DBG2OLC assembly completed."
-
-# consensus stage
-echo "Preparing reads in single fasta..."
-cat "${SOAP_ASSEMBELLY}" "HS7_pacbioData.fasta" > "DBG2OLC_contigs_plus_pacbio.fasta"
-
-# Enable the use of my_split_nrun_sparc and pitchfork
-echo "Setting up environment for Sparc polishing..."
-singularity exec ${SINGULARITY} \
-    bash -c "chmod a+x ${WORKING_FOLDER}/scripts/my_split_nrun_sparc.sh \
-    && source /pitchfork/setup-env.sh"
-
-
-echo "Running my_split_nrun_sparc.sh..."
-singularity exec "${SINGULARITY}" \
-    ${WORKING_FOLDER}/scripts/my_split_nrun_sparc.sh \
-    backbone_raw.fasta \
-    DBG2OLC_Consensus_info.txt \
-    DBG2OLC_contigs_plus_pacbio.fasta \
-    consensus_output 2
-
-
-# clean up intermediate files
-rm "HS7_pacbioData.fasta"
 # Completion message
 echo "Done"
 date
