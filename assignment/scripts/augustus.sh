b6f3bd77ad45c82acf07fbd2e0fb2af884bce0f1
Matthew Spriggs <matspriggs@gmail.com>
2026-01-26 08:53:05 +0000

    add Augustus qsub script for gene prediction and gene counting

---
+++ assignment/scripts/augustus.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Boilerplate qsub script template
+# Version 1.0.0, 17Jan2025
+# Matthew Spriggs
+
+# Crescent2 script
+# Note: this script should be run on a compute node
+# Note: output and error logs will be stored in the logs folder, this folder must exist
+# qsub script.sh
+
+# PBS directives
+#---------------
+
+#PBS -N augustus
+#PBS -l nodes=1:ncpus=4
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M Matthew.Spriggs.452@cranfield.ac.uk
+#PBS -o /mnt/beegfs/home/s430452/advanced_sequencing_informatics_and_genome_assembly/assignment/logs
+
+#===============
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps/modules/all
+
+##  Load the default application environment
+## Purge existing apps
+module purge all
+# module load <YOUR_REQUIRED_MODULES>
+## =============
+
+# --- Your code starts here --- #
+
+# Stop at runtime errors
+set -e
+
+# Start message (this is an example, change it!)
+echo "Started script"
+date
+echo ""
+
+# Load required modules (this is an example, change it if needed!)
+module load Singularity/3.11.0-1-system
+module load AUGUSTUS/3.5.0-foss-2023a
+singularity --version
+
+# Working folder (this is an example, change it if needed!)
+WORKING_FOLDER="/mnt/beegfs/home/s430452/advanced_sequencing_informatics_and_genome_assembly/assignment/"
+cd "${WORKING_FOLDER}"
+
+# Load static file paths 
+source "${WORKING_FOLDER}/scripts/filepaths.txt"
+
+# Any additional variables or paths can be added here
+#<-- your variables ...>
+    
+# Main code 
+# ========================
+echo "Running AUGUSTUS gene prediction on assembly: ${MASURCA_POLISH_ASSEMBELLY}"
+augustus \
+    --species=human \
+    --gff3=on \
+    "${MASURCA_POLISH_ASSEMBELLY}" > "/mnt/beegfs/home/s430452/advanced_sequencing_informatics_and_genome_assembly/assignment/annotation/masurca_genes.gff3"
+
+echo "Gene prediction completed. Output saved to masurca_genes.gff3"
+
+echo "Counting predicted genes..."
+# Count genes
+gene_count=$(grep -c $'\tgene\t' /mnt/beegfs/home/s430452/advanced_sequencing_informatics_and_genome_assembly/assignment/annotation/masurca_genes.gff3)
+echo "Total predicted genes: ${gene_count}"
+
+# Completion message
+echo "Done"
+date
+
+# Do not forget to update the header and PBS directives:
+# e.g. the job name, e-mail address,
+# num of cores, required time and cluster queue, 
+# Also, do not to forget to load and check the required modules.
+
+## Tidy up
+## =========================
+rm $PBS_O_WORKDIR/$PBS_JOBID
